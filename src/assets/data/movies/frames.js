fe38a25b38481068539fb8791a5322f0003108a1
vxll <vell3.2@yandex.ru>
2025-04-18 18:10:44 +0500

    добавлено наполнение 1941 (нужно подправить Мечту)

--- src/assets/data/movies/frames.js
+++ src/assets/data/movies/frames.js
@@ -1,82 +1,86 @@
 // Импорты кадров фильмов
-import frame1941_1_1 from '../../images/movieFrames/1941-1-1.webp';
-import frame1941_1_2 from '../../images/movieFrames/1941-1-2.webp';
-import frame1941_1_3 from '../../images/movieFrames/1941-1-3.webp';
-import frame1941_1_4 from '../../images/movieFrames/1941-1-4.webp';
-import frame1941_2_1 from '../../images/movieFrames/1941-2-1.webp';
-import frame1941_2_2 from '../../images/movieFrames/1941-2-2.webp';
-import frame1941_3_1 from '../../images/movieFrames/1941-3-1.webp';
-import frame1941_3_2 from '../../images/movieFrames/1941-3-2.webp';
-import frame1941_3_3 from '../../images/movieFrames/1941-3-3.webp';
-import frame1941_3_4 from '../../images/movieFrames/1941-3-4.webp';
-import frame1941_3_5 from '../../images/movieFrames/1941-3-5.webp';
-import frame1941_3_6 from '../../images/movieFrames/1941-3-6.webp';
-import frame1941_3_7 from '../../images/movieFrames/1941-3-7.webp';
-import frame1941_3_8 from '../../images/movieFrames/1941-3-8.webp';
-import frame1941_3_9 from '../../images/movieFrames/1941-3-9.webp';
-import frame1941_3_10 from '../../images/movieFrames/1941-3-10.webp';
-import frame1941_4_1 from '../../images/movieFrames/1941-4-1.webp';
-import frame1942_1_1 from '../../images/movieFrames/1942-1-1.webp';
-import frame1942_3_1 from '../../images/movieFrames/1942-3-1.webp';
-import frame1942_3_2 from '../../images/movieFrames/1942-3-2.webp';
-import frame1942_3_3 from '../../images/movieFrames/1942-3-3.webp';
-import frame1942_3_4 from '../../images/movieFrames/1942-3-4.webp';
-import frame1942_3_5 from '../../images/movieFrames/1942-3-5.webp';
-import frame1942_4_1 from '../../images/movieFrames/1942-4-1.webp';
-import frame1942_4_2 from '../../images/movieFrames/1942-4-2.webp';
-import frame1942_4_3 from '../../images/movieFrames/1942-4-3.webp';
-import frame1942_4_4 from '../../images/movieFrames/1942-4-4.webp';
-import frame1942_4_5 from '../../images/movieFrames/1942-4-5.webp';
-import frame1942_4_6 from '../../images/movieFrames/1942-4-6.webp';
-import frame1942_4_7 from '../../images/movieFrames/1942-4-7.webp';
-import frame1942_4_8 from '../../images/movieFrames/1942-4-8.webp';
-import frame1942_4_9 from '../../images/movieFrames/1942-4-9.webp';
-import frame1942_4_10 from '../../images/movieFrames/1942-4-10.webp';
-import frame1942_4_11 from '../../images/movieFrames/1942-4-11.webp';
-import frame1942_4_12 from '../../images/movieFrames/1942-4-12.webp';
-import frame1943_1_1 from '../../images/movieFrames/1943-1-1.webp';
-import frame1943_1_2 from '../../images/movieFrames/1943-1-2.webp';
-import frame1943_1_3 from '../../images/movieFrames/1943-1-3.webp';
-import frame1943_1_4 from '../../images/movieFrames/1943-1-4.webp';
+import frame1941_1_1 from "../../images/movieFrames/1941-1-1.webp";
+import frame1941_1_2 from "../../images/movieFrames/1941-1-2.webp";
+import frame1941_1_3 from "../../images/movieFrames/1941-1-3.webp";
+import frame1941_1_4 from "../../images/movieFrames/1941-1-4.webp";
+import frame1941_2_1 from "../../images/movieFrames/1941-2-1.webp";
+import frame1941_2_2 from "../../images/movieFrames/1941-2-2.webp";
+import frame1941_3_1 from "../../images/movieFrames/1941-3-1.webp";
+import frame1941_3_2 from "../../images/movieFrames/1941-3-2.webp";
+import frame1941_3_3 from "../../images/movieFrames/1941-3-3.webp";
+import frame1941_3_4 from "../../images/movieFrames/1941-3-4.webp";
+import frame1941_3_5 from "../../images/movieFrames/1941-3-5.webp";
+import frame1941_3_6 from "../../images/movieFrames/1941-3-6.webp";
+import frame1941_3_7 from "../../images/movieFrames/1941-3-7.webp";
+import frame1941_3_8 from "../../images/movieFrames/1941-3-8.webp";
+import frame1941_3_9 from "../../images/movieFrames/1941-3-9.webp";
+import frame1941_3_10 from "../../images/movieFrames/1941-3-10.webp";
+import frame1941_4_1 from "../../images/movieFrames/1941-4-1.webp";
+import frame1942_1_1 from "../../images/movieFrames/1942-1-1.webp";
+import frame1942_3_1 from "../../images/movieFrames/1942-3-1.webp";
+import frame1942_3_2 from "../../images/movieFrames/1942-3-2.webp";
+import frame1942_3_3 from "../../images/movieFrames/1942-3-3.webp";
+import frame1942_3_4 from "../../images/movieFrames/1942-3-4.webp";
+import frame1942_3_5 from "../../images/movieFrames/1942-3-5.webp";
+import frame1942_4_1 from "../../images/movieFrames/1942-4-1.webp";
+import frame1942_4_2 from "../../images/movieFrames/1942-4-2.webp";
+import frame1942_4_3 from "../../images/movieFrames/1942-4-3.webp";
+import frame1942_4_4 from "../../images/movieFrames/1942-4-4.webp";
+import frame1942_4_5 from "../../images/movieFrames/1942-4-5.webp";
+import frame1942_4_6 from "../../images/movieFrames/1942-4-6.webp";
+import frame1942_4_7 from "../../images/movieFrames/1942-4-7.webp";
+import frame1942_4_8 from "../../images/movieFrames/1942-4-8.webp";
+import frame1942_4_9 from "../../images/movieFrames/1942-4-9.webp";
+import frame1942_4_10 from "../../images/movieFrames/1942-4-10.webp";
+import frame1942_4_11 from "../../images/movieFrames/1942-4-11.webp";
+import frame1942_4_12 from "../../images/movieFrames/1942-4-12.webp";
+import frame1943_1_1 from "../../images/movieFrames/1943-1-1.webp";
+import frame1943_1_2 from "../../images/movieFrames/1943-1-2.webp";
+import frame1943_1_3 from "../../images/movieFrames/1943-1-3.webp";
+import frame1943_1_4 from "../../images/movieFrames/1943-1-4.webp";
+import frame1941_2_3 from "../../images/movieFrames/1941-2-3.webp";
+import frame1941_2_4 from "../../images/movieFrames/1941-2-4.webp";
 
 export const movieFrames = {
-  '1941-1-1': frame1941_1_1,
-  '1941-1-2': frame1941_1_2,
-  '1941-1-3': frame1941_1_3,
-  '1941-1-4': frame1941_1_4,
-  '1941-2-1': frame1941_2_1,
-  '1941-2-2': frame1941_2_2,
-  '1941-3-1': frame1941_3_1,
-  '1941-3-2': frame1941_3_2,
-  '1941-3-3': frame1941_3_3,
-  '1941-3-4': frame1941_3_4,
-  '1941-3-5': frame1941_3_5,
-  '1941-3-6': frame1941_3_6,
-  '1941-3-7': frame1941_3_7,
-  '1941-3-8': frame1941_3_8,
-  '1941-3-9': frame1941_3_9,
-  '1941-3-10': frame1941_3_10,
-  '1941-4-1': frame1941_4_1,
-  '1942-1-1': frame1942_1_1,
-  '1942-3-1': frame1942_3_1,
-  '1942-3-2': frame1942_3_2,
-  '1942-3-3': frame1942_3_3,
-  '1942-3-4': frame1942_3_4,
-  '1942-3-5': frame1942_3_5,
-  '1942-4-1': frame1942_4_1,
-  '1942-4-2': frame1942_4_2,
-  '1942-4-3': frame1942_4_3,
-  '1942-4-4': frame1942_4_4,
-  '1942-4-5': frame1942_4_5,
-  '1942-4-6': frame1942_4_6,
-  '1942-4-7': frame1942_4_7,
-  '1942-4-8': frame1942_4_8,
-  '1942-4-9': frame1942_4_9,
-  '1942-4-10': frame1942_4_10,
-  '1942-4-11': frame1942_4_11,
-  '1942-4-12': frame1942_4_12,
-  '1943-1-1': frame1943_1_1,
-  '1943-1-2': frame1943_1_2,
-  '1943-1-3': frame1943_1_3,
-  '1943-1-4': frame1943_1_4
-}; 
+  "1941-1-1": frame1941_1_1,
+  "1941-1-2": frame1941_1_2,
+  "1941-1-3": frame1941_1_3,
+  "1941-1-4": frame1941_1_4,
+  "1941-2-1": frame1941_2_1,
+  "1941-2-2": frame1941_2_2,
+  "1941-2-3": frame1941_2_3,
+  "1941-2-4": frame1941_2_4,
+  "1941-3-1": frame1941_3_1,
+  "1941-3-2": frame1941_3_2,
+  "1941-3-3": frame1941_3_3,
+  "1941-3-4": frame1941_3_4,
+  "1941-3-5": frame1941_3_5,
+  "1941-3-6": frame1941_3_6,
+  "1941-3-7": frame1941_3_7,
+  "1941-3-8": frame1941_3_8,
+  "1941-3-9": frame1941_3_9,
+  "1941-3-10": frame1941_3_10,
+  "1941-4-1": frame1941_4_1,
+  "1942-1-1": frame1942_1_1,
+  "1942-3-1": frame1942_3_1,
+  "1942-3-2": frame1942_3_2,
+  "1942-3-3": frame1942_3_3,
+  "1942-3-4": frame1942_3_4,
+  "1942-3-5": frame1942_3_5,
+  "1942-4-1": frame1942_4_1,
+  "1942-4-2": frame1942_4_2,
+  "1942-4-3": frame1942_4_3,
+  "1942-4-4": frame1942_4_4,
+  "1942-4-5": frame1942_4_5,
+  "1942-4-6": frame1942_4_6,
+  "1942-4-7": frame1942_4_7,
+  "1942-4-8": frame1942_4_8,
+  "1942-4-9": frame1942_4_9,
+  "1942-4-10": frame1942_4_10,
+  "1942-4-11": frame1942_4_11,
+  "1942-4-12": frame1942_4_12,
+  "1943-1-1": frame1943_1_1,
+  "1943-1-2": frame1943_1_2,
+  "1943-1-3": frame1943_1_3,
+  "1943-1-4": frame1943_1_4,
+};
